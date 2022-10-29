defde1c1efc0715332c46f31c93188516494a52a
Glazapolzet <VypZlol@yandex.ru>
2022-10-29 16:53:52 +0300

    LR5

---
+++ LR5/src/main/java/org/example/AStarState.java
@@ -0,0 +1,102 @@
+package org.example;
+
+import java.util.HashMap;
+
+/**
+ * This class stores the basic state necessary for the A* algorithm to compute a
+ * path across a map.  This state includes a collection of "open waypoints" and
+ * another collection of "closed waypoints."  In addition, this class provides
+ * the basic operations that the A* pathfinding algorithm needs to perform its
+ * processing.
+ **/
+public class AStarState
+{
+    /** This is a reference to the map that the A* algorithm is navigating. **/
+    private Map2D map;
+
+    private HashMap<Location, Waypoint> waypointsOpened = new HashMap<>();
+    private HashMap<Location, Waypoint> waypointsClosed = new HashMap<>();
+
+    /**
+     * Initialize a new state object for the A* pathfinding algorithm to use.
+     **/
+    public AStarState(Map2D map)
+    {
+        if (map == null)
+            throw new NullPointerException("map cannot be null");
+
+        this.map = map;
+    }
+
+    /** Returns the map that the A* pathfinder is navigating. **/
+    public Map2D getMap()
+    {
+        return map;
+    }
+
+    /**
+     * This method scans through all open waypoints, and returns the waypoint
+     * with the minimum total cost.  If there are no open waypoints, this method
+     * returns <code>null</code>.
+     **/
+    public Waypoint getMinOpenWaypoint()
+    {
+        if(waypointsOpened.isEmpty()) {
+            return null;
+        }
+
+        Waypoint minOpenWP = (Waypoint) waypointsOpened.values().toArray()[0];
+
+        for(Waypoint openWP : waypointsOpened.values()) {
+            if (openWP.getTotalCost() < minOpenWP.getTotalCost()) {
+                minOpenWP = openWP;
+            }
+        }
+        return minOpenWP;
+    }
+
+    /**
+     * This method adds a waypoint to (or potentially updates a waypoint already
+     * in) the "open waypoints" collection.  If there is not already an open
+     * waypoint at the new waypoint's location then the new waypoint is simply
+     * added to the collection.  However, if there is already a waypoint at the
+     * new waypoint's location, the new waypoint replaces the old one <em>only
+     * if</em> the new waypoint's "previous cost" value is less than the current
+     * waypoint's "previous cost" value.
+     **/
+    public boolean addOpenWaypoint(Waypoint newWP)
+    {
+        if (!waypointsOpened.containsKey(newWP.getLocation()) || waypointsOpened.get(newWP.getLocation()).getPreviousCost() > newWP.getPreviousCost()) {
+            waypointsOpened.put(newWP.getLocation(), newWP);
+        }
+        return waypointsOpened.get(newWP.getLocation()).getPreviousCost() == newWP.getPreviousCost();
+    }
+
+
+    /** Returns the current number of open waypoints. **/
+    public int numOpenWaypoints()
+    {
+        return waypointsOpened.size();
+    }
+
+
+    /**
+     * This method moves the waypoint at the specified location from the
+     * open list to the closed list.
+     **/
+    public void closeWaypoint(Location loc)
+    {
+        waypointsClosed.put(loc, waypointsOpened.get(loc));
+        waypointsOpened.remove(loc);
+    }
+
+    /**
+     * Returns true if the collection of closed waypoints contains a waypoint
+     * for the specified location.
+     **/
+    public boolean isLocationClosed(Location loc)
+    {
+        return waypointsClosed.containsKey(loc);
+    }
+}
+
